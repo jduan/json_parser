4d299940de778e10c720d0252bb7899d85c0d0dd
Jingjing Duan <jduan@twitter.com>
2014-03-26 10:56:54 -0700

    fail a test on purpose

--- spec/json_parser/calculator_spec.rb
+++ spec/json_parser/calculator_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 module JsonParser
   describe Calculator do
     it "#add should sum up 2 numbers" do
-      Calculator.new.add(1, 2).should == 3
+      Calculator.new.add(1, 2).should == 30
     end
   end
 end
